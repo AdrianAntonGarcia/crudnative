839211cb84c5909f1fb0256633f4dc084249b272
adriton <adrtler@gmail.com>
2021-04-11 18:20:52 +0200

    Añadir los demás campos del formulario de nuevo cliente

--- views/NuevoCliente.js
+++ views/NuevoCliente.js
@@ -4,12 +4,45 @@ import {TextInput, Headline, Button} from 'react-native-paper';
 import globalStyles from '../styles/global';
 
 export const NuevoCliente = () => {
+  const leerNombre = () => {
+    console.log('Escribiendo el nombre');
+  };
   return (
     <View style={globalStyles.contenerdor}>
       <Headline style={globalStyles.titulo}>Añadir nuevo Cliente</Headline>
-      <TextInput />
+      <TextInput
+        label="Nombre"
+        placeholder="Adri"
+        onChangeText={() => leerNombre()}
+        style={styles.input}
+      />
+      <TextInput
+        label="Teléfono"
+        placeholder="24324324"
+        onChangeText={() => leerNombre()}
+        style={styles.input}
+      />
+      <TextInput
+        label="Correo"
+        placeholder="correo@correo.com"
+        onChangeText={() => leerNombre()}
+        style={styles.input}
+      />
+      <TextInput
+        label="Empresa"
+        placeholder="Nombre Empresa"
+        onChangeText={() => leerNombre()}
+        style={styles.input}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  input: {
+    marginBottom: 20,
+    backgroundColor: 'transparent',
+  },
+});
 
 export default NuevoCliente;
