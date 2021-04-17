86ec68307d05c8c3c6bd4e0348db25ee51309e2c
adriton <adrtler@gmail.com>
2021-04-17 09:55:09 +0200

    Mostrando la información del cliente

--- views/DetallesCliente.js
+++ views/DetallesCliente.js
@@ -1,9 +1,30 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {View, StyleSheet} from 'react-native';
+import {Headline, Text, Subheading, Button} from 'react-native-paper';
+import globalStyles from '../styles/global';
 
 export const DetallesCliente = ({route}) => {
-  console.log(route.params);
-  return <Text>Desde Nuevo Cliente</Text>;
+  const {nombre, telefono, correo, empresa} = route.params.item;
+  return (
+    <View style={globalStyles.contenerdor}>
+      <Headline style={globalStyles.titulo}>{nombre}</Headline>
+      <Text style={styles.texto}>
+        Empresa: <Subheading>{empresa}</Subheading>{' '}
+      </Text>
+      <Text style={styles.texto}>
+        Correo: <Subheading>{correo}</Subheading>{' '}
+      </Text>
+      <Text style={styles.texto}>
+        Teléfono: <Subheading>{telefono}</Subheading>{' '}
+      </Text>
+      <Button>Eliminar Cliente</Button>
+    </View>
+  );
 };
-
+const styles = StyleSheet.create({
+  texto: {
+    marginBottom: 20,
+    fontSize: 18,
+  },
+});
 export default DetallesCliente;
